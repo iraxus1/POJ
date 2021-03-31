78810e77802bce48ae6362c323e25fc7ae6c6c0e
iraxus1 <76206932+iraxus1@users.noreply.github.com>
2021-03-31 11:54:58 +0200

    Update Main3.java

--- ZAD 3/ZAD2/Main3.java
+++ ZAD 3/ZAD2/Main3.java
@@ -7,8 +7,10 @@ public class Main3 {
         Random rand = new Random();
         Points c1 = new Points(rand.nextInt(10),rand.nextInt(10));
         System.out.println(c1.Location());
+        
         Points c2 = new Points(rand.nextInt(10),rand.nextInt(10));
         System.out.println(c2.Location());
+        
         System.out.println(c1.Length(c2.xValue(),c2.yValue()));
     }
 }
